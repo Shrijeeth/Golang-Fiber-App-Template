1ab0500b9c0814c9305233463ddb1e14b884776a
SHRIJEETH\shrij <shrijeethsuresh@gmail.com>
2023-09-28 00:04:30 +0530

    Optimized aws s3 connection along with credentials

--- platform/cloud_object_storage/aws_s3.go
+++ platform/cloud_object_storage/aws_s3.go
@@ -2,14 +2,30 @@ package cloud_storage_object
 
 import (
 	"github.com/aws/aws-sdk-go/aws"
-    "github.com/aws/aws-sdk-go/aws/session"
-    "github.com/aws/aws-sdk-go/service/s3"
+	"github.com/aws/aws-sdk-go/aws/credentials"
+	"github.com/aws/aws-sdk-go/aws/session"
+	"github.com/aws/aws-sdk-go/service/s3"
+	"os"
 )
 
 func AwsS3Connect() (*s3.S3, error) {
-	sess, err := session.NewSession(&aws.Config{ // TODO: Add correct configs
-		Region: aws.String("us-west-2")},
-	)
+	s3Id := os.Getenv("AWS_ACCESS_KEY")
+	s3Secret := os.Getenv("AWS_SECRET_KEY")
+	s3Endpoint := os.Getenv("AWS_S3_ENDPOINT")
+	awsRegion := os.Getenv("AWS_REGION")
+	awsProfile := os.Getenv("AWS_PROFILE")
+
+	s3Config := aws.Config{
+		Credentials:      credentials.NewStaticCredentials(s3Id, s3Secret, ""),
+		Endpoint:         aws.String(s3Endpoint),
+		Region:           aws.String(awsRegion),
+		S3ForcePathStyle: aws.Bool(true),
+	}
+
+	sess, err := session.NewSessionWithOptions(session.Options{
+		Config:  s3Config,
+		Profile: awsProfile,
+	})
 	if err != nil {
 		return nil, err
 	}
